007a577da27ceb68f4ce030a646656142fb8ac00
Pavel Lepikhov <106874030+pavel26724@users.noreply.github.com>
2022-06-18 12:49:10 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,21 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+
+import random
+
+numbers = []
+n = 4
+i = 0
+
+for i in range(n):
+    number = random.randint(-10, 10)
+    numbers.append(number)
+print(numbers)
+
+summa = 0
+for number in numbers:
+    if number > 0:
+        summa += number
+
+print(summa)
