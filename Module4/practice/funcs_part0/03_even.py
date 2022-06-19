edbdc13b40a64ceacce132149202735ba4c898ae
Pavel Lepikhov <106874030+pavel26724@users.noreply.github.com>
2022-06-19 11:27:02 +0300

    Update 03_even.py

--- Module4/practice/funcs_part0/03_even.py
+++ Module4/practice/funcs_part0/03_even.py
@@ -7,3 +7,14 @@ if even(n):
    print("Число четное")
 else:
    print("Число не четное")
+
+
+def even(n):
+    if n % 2 == 0:
+        return (n)
+
+n = int(input('Введи число с клавиатуры: '))
+if even(n):
+    print("Число четное")
+else:
+    print("Число не четное")
