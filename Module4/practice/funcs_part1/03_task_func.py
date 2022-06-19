652487723f88feb15211fa79d9609a0483c87d4d
Pavel Lepikhov <106874030+pavel26724@users.noreply.github.com>
2022-06-19 11:55:21 +0300

    Update 03_task_func.py

--- Module4/practice/funcs_part1/03_task_func.py
+++ Module4/practice/funcs_part1/03_task_func.py
@@ -10,3 +10,12 @@ def distance(x1, y1, x2, y2):
 print(distance(2, 4, 2, 9))
 print(distance(12, 8, 12, -9))
 print(distance(23, 0, 15, 32))
+
+def distance(x1, y1, x2, y2):
+    length = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    return (length)
+
+# Тестируем функцию
+print(distance(2, 2, 2, 9))
+print(distance(12, 8, 12, -9))
+print(distance(23, 0, 15, 32))
