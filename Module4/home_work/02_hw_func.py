a5177db60784333641d80e707d6217fb46a44332
Pavel Lepikhov <106874030+pavel26724@users.noreply.github.com>
2022-06-19 20:40:55 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -11,3 +11,23 @@ def distance(x1, y1, x2, y2):
 
 # TODO: your code here
 print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+
+
+def distance(xa, ya, xb, yb, xc, yc):
+    length_ab = ((xb - xa) ** 2 + (yb - ya) ** 2) ** 0.5
+    length_bc = ((xc - xb) ** 2 + (yc - yb) ** 2) ** 0.5
+    length_ac = ((xc - xa) ** 2 + (yc - ya) ** 2) ** 0.5
+    if length_ab < length_bc and length_ab < length_ac:
+        length_ab = 'AB'
+        return length_ab
+    elif length_bc < length_ab and length_bc < length_ac:
+        length_bc = 'BC'
+        return length_bc
+    else:
+        length_ac = 'AC'
+        return length_ac
+
+
+distance(6, 10, 1332, 2025, 4, 222)
+# TODO: your code here
+print("Самый короткий отрезок:", distance(6, 10, 1332, 2025, 4, 222))  # Выводим название отрезка, например “АС”.
