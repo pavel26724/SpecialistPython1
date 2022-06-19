580e960801aa2cc120c68e85e45d1a154a2f50e0
Pavel Lepikhov <106874030+pavel26724@users.noreply.github.com>
2022-06-19 11:01:21 +0300

    Update 01_my_abs.py

--- Module4/practice/funcs_part0/01_my_abs.py
+++ Module4/practice/funcs_part0/01_my_abs.py
@@ -6,3 +6,13 @@ def my_abs(...):
 print(my_abs(-5))
 print(my_abs(5))
 print(my_abs(0))
+
+
+def my_abs(n):
+    if n < 0:
+        return -n
+    return n
+
+print(my_abs(-19))
+print(my_abs(-11115))
+print(my_abs(-0.25))
