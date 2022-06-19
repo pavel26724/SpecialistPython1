b7eb9bf4226116662392bdfbe3b960489477d183
Pavel Lepikhov <106874030+pavel26724@users.noreply.github.com>
2022-06-19 20:05:18 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -11,3 +11,16 @@ def lucky_ticket(ticket_number):
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+
+def lucky_ticket(ticket_number):
+    if int(str(ticket_number)[0]) + int(str(ticket_number)[1]) + int(str(ticket_number)[2]) \
+            == int(str(ticket_number)[-1]) + int(str(ticket_number)[-2]) + int(str(ticket_number)[-3]):
+        return ('Билет счастливый')
+    else:
+        return ('Билет НЕсчастливый')
+
+
+# Тестируем функцию
+print(lucky_ticket(123312))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
