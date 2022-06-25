addcc57db945ea572b411f86ee1b780bb4e578a5
Pavel Lepikhov <106874030+pavel26724@users.noreply.github.com>
2022-06-25 11:44:46 +0300

    Update 10_task_func.py

--- Module4/practice/funcs_part2/10_task_func.py
+++ Module4/practice/funcs_part2/10_task_func.py
@@ -9,3 +9,14 @@ def average(*args):
 print(average(3, 4, 8))
 print(average(1, 4, 5, -3, 8, 4))
 print(average(-10, 12, 6.3, -5.5, 7, 0.2))
+
+def average(*args):
+    s = 0
+    for el in args:
+        s += el
+    return s / len(args)
+
+
+print(average(3, 4, 8))
+print(average(1, 4, 5, -3, 8, 4))
+print(average(-10, 12, 6.3, -5.5, 7, 0.2))
