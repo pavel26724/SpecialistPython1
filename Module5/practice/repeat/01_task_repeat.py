e898f4b2b6d5a4289b665024f9f802fdade01ad4
Pavel Lepikhov <106874030+pavel26724@users.noreply.github.com>
2022-06-26 13:15:59 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -5,3 +5,17 @@
 def gen_list(size, of, to):
     pass
 
+import random
+
+def gen_list(size, of, to):
+    numbers = []
+    i = 0
+
+    while i < size:
+        number = random.randint(of, to)
+        numbers.append(number)
+        i += 1
+    print(numbers)
+    # return gen_list
+
+gen_list(11, -100, 100)
